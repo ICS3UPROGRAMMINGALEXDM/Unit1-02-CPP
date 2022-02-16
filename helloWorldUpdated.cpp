7dcbdea4d629be635c48b55c96dd8fb359968cb3
Ubuntu <ubuntu@ip-172-31-86-29.ec2.internal>
2022-02-16 14:25:40 +0000

    Finished helloWorldUpdated

---
+++ helloWorldUpdated.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello Ms. Raffin!" << std::endl;
+	cout << "So far, I have really enjoyed this ICS3U class." << std::endl;
+	
+}
